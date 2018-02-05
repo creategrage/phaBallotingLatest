0f46ffe286da2334bb3decd9d76be202ee85bf3d
Ejaz Musavi <Ejaz Musavi@DESKTOP-VKE0RLI>
2018-02-05 13:00:55 +0500

    initial uploads

---
+++ phaBalloting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(phaBalloting.Startup))]
+namespace phaBalloting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
